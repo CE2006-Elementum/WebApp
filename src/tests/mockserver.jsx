789cb3621df701797c1edec45680cde53d0c084f
Zhi Heng Loh <77670828+zhiheng97@users.noreply.github.com>
2022-04-10 13:39:43 +0800

    Minor Fixes

--- src/tests/mockserver.jsx
+++ src/tests/mockserver.jsx
@@ -8,21 +8,21 @@ import { searchRes, valRes } from './testdata.jsx';
  * @author Zhi Heng
  */
 export const server = setupServer(
-    rest.post('http://127.0.0.1:43325/search', (req, res, ctx) => {
+    rest.post('http://localhost:43325/search', (req, res, ctx) => {
         if(req.body.flat_properties.price_range.max > 0.1)
             return res(ctx.status(200), ctx.json(searchRes));
         else return res(ctx.status(200), ctx.json({blocks: []}));
     }),
-    rest.post('http://127.0.0.1:43325/valuation', (req, res, ctx) => {
+    rest.post('http://localhost:43325/valuation', (req, res, ctx) => {
         return res(ctx.status(200), ctx.json(valRes));
     }),
-    rest.get('http://127.0.0.1:43325/mapskey', (req, res, ctx) => {
+    rest.get('http://localhost:43325/mapskey', (req, res, ctx) => {
         return res(ctx.status(200));
     }),
-    rest.post('http://127.0.0.1:43325/feedback', (req, res, ctx) => {
+    rest.post('http://localhost:43325/feedback', (req, res, ctx) => {
         return res(ctx.status(200));
     }),
-    rest.post('http://127.0.0.1:43325/*', (req, res, ctx) => {
+    rest.post('http://localhost:43325/*', (req, res, ctx) => {
         return rest(ctx.status(404));
     })
 )
